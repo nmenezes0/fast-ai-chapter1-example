27fc234cdca36b7508b9771801329730ec369a9e
Nina Menezes <nina.menezes@example.com>
2022-08-24 21:12:07 +0100

    add scripts to get data

---
+++ get_camel_data.py
@@ -0,0 +1,34 @@
+"""Script to get camel images for experimentation."""
+
+from pathlib import Path
+from time import sleep
+
+from duckduckgo_search import ddg_images
+from fastcore.all import L
+from fastai.vision.all import download_images, resize_images
+
+
+PARENT_PATH = Path(__file__).parent
+CAMELS_DATA_PATH = Path(PARENT_PATH, "data/camels")
+CATEGORIES = ["bactrian", "dromedary"]
+
+
+def search_images(term, max_images=30):
+    print(f"Searching for '{term}'")
+    return L(ddg_images(term, max_results=max_images)).itemgot("image")
+
+
+def get_dataset():
+    for category in CATEGORIES:
+        dest = Path(CAMELS_DATA_PATH, category)
+        download_images(dest, urls=search_images(f"{category} camel photo"))
+        sleep(10)  # Pause between searches to avoid over-loading server
+        download_images(dest, urls=search_images(f"{category} camel sun photo"))
+        sleep(10)
+        download_images(dest, urls=search_images(f"{category} camel shade photo"))
+        sleep(10)
+        resize_images(dest, max_size=400, dest=dest)
+
+
+if __name__ == "__main__":
+    get_dataset()
